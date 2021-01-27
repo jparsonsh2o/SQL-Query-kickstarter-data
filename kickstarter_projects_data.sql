66e79bd13050e9c852e0151959ea897bb2e341be
Jessica Parsons <jparsonsh2o@gmail.com>
2021-01-27 09:07:56 -0600

    Add files via upload

---
+++ kickstarter_projects_data.sql
@@ -0,0 +1,46 @@
+-- How many distinct values are in the "country" field?
+SELECT COUNT(DISTINCT country)
+FROM ksprojects;
+
+-- Write a query that returns the average number of backers per main_category, 
+-- rounded to the nearest whole number and sorted from high to low. 
+-- What is the numerical value of the first row in the returned dataset?
+
+SELECT main_category, ROUND(AVG(backers),0) AS avg_backers
+FROM ksprojects
+GROUP BY main_category
+ORDER BY avg_backers DESC;
+
+-- Write a query that shows, for each category, how many campaigns were successful 
+-- and the average difference per project between dollars pledged 
+-- and the goal for successful campaigns in that category. 
+-- Which category raised the most (pledged) above and beyond its goal amount?
+
+SELECT category, AVG(usd_pledged - goal) avg_delta
+FROM ksprojects
+WHERE state = 'successful'
+GROUP BY category
+ORDER BY avg_delta DESC;
+
+-- Write a query that shows, for each main_category, 
+-- how many successful projects had between 5 and 10 backers. 
+-- How many "Publishing" projects meet this criteria? 
+
+SELECT main_category, COUNT(*) AS num_successful
+FROM ksprojects
+WHERE backers BETWEEN 5 AND 10
+AND state = 'successful'
+GROUP BY main_category;
+
+-- Excluding Games and Technology records in the main_category field, 
+-- return the total of all backers for successful projects by main_category 
+-- where the total number of backers was over 100,000.  
+-- Sort by main_category from A-Z. How many backers does the first record have?
+
+SELECT main_category, SUM(backers) AS total_backers
+FROM ksprojects
+WHERE state = 'successful'
+AND main_category NOT IN ('Games','Technology')
+GROUP BY main_category
+HAVING SUM(backers) > 100000
+ORDER BY main_category;
